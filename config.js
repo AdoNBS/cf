bb3faa6100ffa6a0c1a64260b66ca005dd5a7435
Ado <ado.salkic@gmail.com>
2021-02-10 22:35:50 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+function samoPokretanjeRT() {
+    console.clear();
+    console.log('Samo pokretanje...');
+    fbeContainer_div = document.getElementById("fbe-container");
+    
+    if(fbeContainer_div){
+       
+        console.log('Potrebno...');
+        document.getElementById("fbe-ready").click();
+
+    } else {
+        console.log('Nije potrebno...');
+    }
+    
+}
+window.onload = setTimeout(samoPokretanjeRT, 5000);
+
+
+
+weboffline = null;
+var weboffline = setInterval(function() {
+    if (navigator.onLine || window.onerror) {
+        console.log('Online...');
+       
+    } else {
+        console.log('Offline/Crash...');
+        location.reload(true);
+        clearInterval(weboffline);
+    }
+}, 120000); /* 120000 ~> 2 minutes */
